c31b88f50206e01ebc9b3fe79f13d5b624c236eb
kubawis89 <kubawis89@gmail.com>
2021-02-07 09:42:02 +0100

    express

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const handler = (request, response) => {
+/*const http = require('http');
+const handler = (request, response) => {  //request i response to nazwy umowne mogą być inne np bla1 i bla 2
     response.end("<h1> Hello World </h1>");
     console.log("sample message");
 }
@@ -11,6 +11,21 @@ const port = 3000;
 //})
 
 server.listen(port, (err) => {
+    if (err) {
+        return console.log("Coś poszło nie tak...:", err)
+    }
+    console.log(`Serwer działa na porcie ${port}`);
+})
+*/
+    //   --------------  EXPRESS --------------------------
+const express = require("express"); //express zamiast http
+const port = 3000; //port i app dobrowolne nazwy
+const app = express();
+        //gdy uzytkownik wchodzi na stronę główną
+app.get('/', function (req, res) { //'/' -> adres strony głównej
+    res.send("<h2> Hello World </h2>");
+})
+app.listen(port, (err) => {
     if (err) {
         return console.log("Coś poszło nie tak...:", err)
     }
